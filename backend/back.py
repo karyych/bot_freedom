628fb9e2bd9aa064e04d378348fb7d198dec1d16
karyych <zakhar.karavaev.01@mail.ru>
2024-02-24 13:56:28 +0500

    Add Token API

--- backend/back.py
+++ backend/back.py
@@ -1,3 +1,12 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ParseMode
-from aiogram import executor
+from aiogram import executor 
+
+API_TOKEN = ' '
+
+bot = Bot(token=API_TOKEN)
+
+dp = Dispatcher(bot)
+
+
+
