bc55e15b8dd541c018d297fd7f94030a89a99cac
AkToHuyC <antoha_anton_antoha2@mail.ru>
2024-02-25 17:10:03 +0500

    Добавил запрос валют

--- backend/back.py
+++ backend/back.py
@@ -1,19 +1,55 @@
 import telebot
+import requests
+import json
 
+# Токен
 TOKEN = '6738973994:AAEucnyuxwgdbSQWGsIqpkyUXqW5r-aMai0'
 
-# Экземпляр бота
+# Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
 
-# Обработчик команды /start
+# после params можно тоже вделать форматирование {}, т.к. bbp отвечает только за текущую цену
+REST_API_URL = 'https://tradernet.kz/securities/export?params=bbp&tickers={}'
+
+
+# /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я бот. Как дела?")
 
-# Всё остальное просто возвращаем обратно
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
 
-# Запуск
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text.lower() == 'запрос': # Далее надо будет сделат кнопку валюты и выбор валют, а такая реализация канает на акции
+        bot.send_message(message.chat.id, "Введите тикер:")
+        bot.register_next_step_handler(message, handle_ticker)
+    else:
+        bot.reply_to(message, "Я не понимаю, что вы имеете в виду.")
+
+
+# Функция, добавляющая название тикера в строку REST запроса
+def handle_ticker(message):
+    ticker = message.text.upper() # Может и не надо, надо чекать 
+    response = send_rest_request(REST_API_URL.format(ticker))
+    if response:
+        data = json.loads(response)
+        result = "\n".join([f"{item['c']}: {item['bbp']:.2f}" for item in data])
+        bot.reply_to(message, result)
+    else:
+        bot.reply_to(message, "Ошибка при выполнении запроса.")
+
+
+# Функция для отправки REST запроса
+def send_rest_request(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except Exception as e:
+        return None
+
+# Запускаем бота
 bot.polling()
