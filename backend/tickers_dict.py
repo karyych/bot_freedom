8025c89774a7966eb7599ab3fc6fa0f44137b4dd
karyych <zakhar.karavaev.01@mail.ru>
2024-02-26 20:27:04 +0500

    new_fix

--- backend/tickers_dict.py
+++ backend/tickers_dict.py
@@ -1,10 +1,14 @@
 #Словарь для тикеров 
 
+
+# Курсы валют >> https://tradernet.kz/securities/export?params=ltp&tickers=EUR/KZT вывод по паре 
+# Курсы валют >> https://tradernet.kz/securities/export?params=ltp&tickers=RUB/KZT вывод по паре 
+
 ticker_dict = {
     'CVX': {'ticker': 'CVX.US', 'api': 'https://tradernet.kz/securities/export?params=ltp&tickers={}'},
     'CHEVRON': {'ticker': 'CVX.US', 'api': 'https://tradernet.kz/securities/export?params=ltp&tickers={}'}, 
     'Microsoft': {'ticker': 'MSFT.US', 'api': 'https://tradernet.kz/securities/export?params=ltp&tickers={}'},
     'apple': {'ticker': 'AAPL.US', 'api': 'https://tradernet.kz/securities/export?params=ltp&tickers={}'},
     'aapl': {'ticker': 'AAPL.US', 'api': 'https://tradernet.kz/securities/export?params=ltp&tickers={}'},
-
+    
 }
