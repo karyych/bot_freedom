7b153453bfbee8d8a4fd2dd7f0a678f3edea561f
AkToHuyC <antoha_anton_antoha2@mail.ru>
2024-02-27 21:26:55 +0500

    Коррекция Словаря

--- backend/tickers_dict.py
+++ backend/tickers_dict.py
@@ -1,11 +1,5 @@
 #Словарь для тикеров 
 
-# Курсы валют >> https://tradernet.kz/securities/export?params=ltp&tickers=EUR/KZT вывод по паре 
-# Курсы валют >> https://tradernet.kz/securities/export?params=ltp&tickers=RUB/KZT вывод по паре 
-
-# URL для API запросов
-REST_API_URL = 'https://tradernet.kz/securities/export?params=ltp&tickers={}'
-
 ticker_dict = {
     # Вывод Великолепной Семёрки
     'Nvidia': {'ticker': 'NVDA.US'},
